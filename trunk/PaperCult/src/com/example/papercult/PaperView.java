6001dfea77e0d3af8e022900901f502bfadfb1eb
binan1 <binan1@21ed9746-ec09-4e3c-9cd8-fa4dcd8b6b0b>
2013-03-20 06:46:44 +0000

    여러번 접기 만드는중

--- trunk/PaperCult/src/com/example/papercult/PaperView.java
+++ trunk/PaperCult/src/com/example/papercult/PaperView.java
@@ -1,34 +1,43 @@
 package com.example.papercult;
 
+import java.util.Vector;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.PointF;
-
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Toast;
+
 
 public class PaperView extends View {
-	Context con;
+	Vector<Polygon> poly = new Vector<Polygon>();
+	Vector<Polygon> foldPoly = new Vector<Polygon>();
+	Vector<Polygon> temp  = new Vector<Polygon>();
+	
 	PointF touchStart = new PointF();
 	PointF touchEnd = new PointF();
+	
 	boolean click = false;
 	
 	public PaperView(Context context) {
 		super(context);
-		con = context;
 		
+		Polygon p = new Polygon();
+		p.add(new PointF(100,100));
+		p.add(new PointF(400,100));
+		p.add(new PointF(400,400));
+		p.add(new PointF(100,400));
+		poly.add(p);
 	}
 	
 	public boolean onTouchEvent(MotionEvent event){
 		if (event.getAction() == MotionEvent.ACTION_DOWN)
 		{
-			click = true;
-			touchStart.x = event.getX();
-			touchStart.y = event.getY();
+			if(click == false){
+				click = true;
+				touchStart.x = event.getX();
+				touchStart.y = event.getY();
+			}
 			return true;
 		}
 		else if(event.getAction() == MotionEvent.ACTION_MOVE)
@@ -36,14 +45,26 @@ public class PaperView extends View {
 			if(click == true){
 				touchEnd.x = event.getX();
 				touchEnd.y = event.getY();
+				
+				PointF start = new PointF(touchStart.x, touchStart.y);
+				PointF end = new PointF(touchEnd.x, touchEnd.y);
+				
+				foldPoly.removeAllElements();
+				for(int i=0; i<poly.size(); i++){
+					foldPoly.add(poly.get(i).cutPolygon(start, end));
+					foldPoly.add(poly.get(i).pullPolygon(start, end));
+				}
 				this.invalidate();
 			}
 			return true;
 		}
 		else if(event.getAction() == MotionEvent.ACTION_UP)
 		{
+			temp = poly;
+			poly = foldPoly;
+			foldPoly = temp;
+			
 			click = false;
-			this.invalidate();
 			return true;
 		}
 		return false;
@@ -52,18 +73,14 @@ public class PaperView extends View {
 	public void onDraw(Canvas canvas){
 		canvas.drawColor(Color.LTGRAY);
 		
-		Polygon p = new Polygon();
-		
-		p.add(new PointF(100,100));
-		p.add(new PointF(200,100));
-		p.add(new PointF(200,200));
-		p.add(new PointF(100,200));
-		
-		Polygon cut = p.cutPolygon(new PointF(touchStart.x, touchStart.y), new PointF(touchEnd.x, touchEnd.y));
-		Polygon pull = p.pullPolygon(new PointF(touchStart.x, touchStart.y), new PointF(touchEnd.x, touchEnd.y));
-		
-		cut.draw(canvas);
-		pull.draw(canvas);
+		if(click == true){
+			for(int i=0; i<foldPoly.size(); i++)
+				foldPoly.get(i).draw(canvas);
+		}
+		else{
+			for(int i=0; i<poly.size(); i++)
+				poly.get(i).draw(canvas);
+		}
 	}
 }
 
