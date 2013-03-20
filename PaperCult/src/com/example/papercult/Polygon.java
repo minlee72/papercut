e42e22d4c92be6453dff372badf84b955265fed5
binan1 <binan1@21ed9746-ec09-4e3c-9cd8-fa4dcd8b6b0b>
2013-03-20 09:32:33 +0000

    폴리곤 클래스 주석 상세히

--- PaperCult/src/com/example/papercult/Polygon.java
+++ PaperCult/src/com/example/papercult/Polygon.java
@@ -28,7 +28,7 @@ public class Polygon {
 	}
 	
 	/**
-	 * 다각형이 터치 입력에 따른 기울기에 기준하여 접혀질때, 접혀지지않는 나머지 부분을 구한다
+	 * 다각형이 터치 입력에 따른 기울기에 기준하여 접혀질때, 접혀지는 부분을 구한다
 	 * @param tlStart	터치입력 시작점
 	 * @param tlEnd   터치입력 끝점
 	 * @return   접혀지지 않은 나머지 다각형
@@ -37,31 +37,31 @@ public class Polygon {
 		if(pointVector.size() < 1)
 			return null;	
 		
-		Polygon result = new Polygon();
+		Polygon result = new Polygon();  //리턴할 결과
 		
 		for (int i = 0; i<(pointVector.size()); i++){
-			PointF hereP = pointVector.get(i);
+			PointF hereP = pointVector.get(i);    //벡터에서 점 하나를 가져온다
 			PointF nextP;
-			if (i != (pointVector.size()-1)){
-				nextP = pointVector.get(i+1);
+			if (i != (pointVector.size()-1)){        //가져온 점이 끝점이 아니면
+				nextP = pointVector.get(i+1);     //다음점도 가져와서 넥스트에 저장
 			}
-			else{
+			else{                                         //가져온 점이 끝점이면 넥스트 값은 0번점. 즉 시작점이 된다
 				nextP = pointVector.get(0);
 			}
 					
-			if(containsPoint(hereP, tlStart, tlEnd) == true){
-				result.add(getSymmetryPoint(hereP, tlStart, tlEnd));
+			if(containsPoint(hereP, tlStart, tlEnd) == true){           //현재 점이 대칭해야될 점이라면
+				result.add(getSymmetryPoint(hereP, tlStart, tlEnd)); //대칭 시킨후 결과 벡터에 추가
 			}
-			PointF crossPoint = getCrossPoint(hereP, nextP, tlStart, tlEnd);
-			if(isInline(pointVector.get(i), nextP, crossPoint) == true){
-				result.add(crossPoint);
+			PointF crossPoint = getCrossPoint(hereP, nextP, tlStart, tlEnd);  //현재 점과 다음점을 잇는 직선과 접혀져야하는 기울기의 교차점
+			if(isInline(pointVector.get(i), nextP, crossPoint) == true){        //현재 점과 다음점의 직선사이로 접혀지는 기울기가 지나간다면
+				result.add(crossPoint);                                                   //그 교차점을 벡터에 추가
 			}
 		}
 		return result;
 	}
 	
 	/**
-	 * 다각형이 터치 입력에 따른 기울기에 기준하여 접혀질때, 접혀지는 부분을 구한다
+	 * 다각형이 터치 입력에 따른 기울기에 기준하여 접혀질때, 접혀지지 않는 나머지 부분을 구한다
 	 * @param tlStart  터치입력 시작점
 	 * @param tlEnd   터치입력 끝점
 	 * @return  접혀지는 다각형
@@ -73,21 +73,21 @@ public class Polygon {
 		Polygon result = new Polygon();
 		
 		for (int i = 0; i<(pointVector.size()); i++){
-			PointF hereP = pointVector.get(i);
+			PointF hereP = pointVector.get(i);        //벡터에서 점을 하나 가져옴
 			PointF nextP;
 			if (i != (pointVector.size()-1)){
-				nextP = pointVector.get(i+1);
-			}
-			else{
+				nextP = pointVector.get(i+1);         
+			}                                                   //벡터에서 다음 점을 하나 가져오는데 
+			else{                                             //지금이 끝점이면 0번점 (시작점)을 가져옴
 				nextP = pointVector.get(0);
 			}
 					
-			if(containsPoint(hereP, tlStart, tlEnd) == false){
-				result.add(hereP);
+			if(containsPoint(hereP, tlStart, tlEnd) == false){  //현재 점이 접혀질때 넘겨지는 점이 아니면
+				result.add(hereP);                                       //결과 벡터에 넣음
 			}
-			PointF crossPoint = getCrossPoint(hereP, nextP, tlStart, tlEnd);
-			if(isInline(pointVector.get(i), nextP, crossPoint) == true){
-				result.add(crossPoint);
+			PointF crossPoint = getCrossPoint(hereP, nextP, tlStart, tlEnd);  
+			if(isInline(pointVector.get(i), nextP, crossPoint) == true){       //현재 점과 다음 점을 잇는 직선에 접는선이 통과하면
+				result.add(crossPoint);                                                  //그 교차점을 찾아서 결과 벡터에 넣음
 			}
 		}
 		return result;
@@ -121,7 +121,7 @@ public class Polygon {
 		Path path = new Path();
 		path.reset();
 		
-		for(int i = 0; i<pointVector.size(); i++){
+		for(int i = 0; i<pointVector.size(); i++){              //벡터에서 점 두개씩 뽑아서 직선 그려줌
 			path.moveTo(pointVector.get(i).x, pointVector.get(i).y);
 			if(i == (pointVector.size()-1))
 				path.lineTo(pointVector.get(0).x, pointVector.get(0).y);
@@ -150,8 +150,8 @@ public class Polygon {
 		
 		float intercept = center.y - (gradient * center.x);
 		
-		if ((tlStart.y-tlEnd.y)==0){   //기울기가 수직일 때 (기울기 값이 0 이여서 계산이 안됨)
-			if(center.x > targetPoint.x){
+		if ((tlStart.y-tlEnd.y)==0){   //기울기가 수직일 때는 x값 만 대칭시켜주면 됨
+			if(center.x > targetPoint.x){      
 				result.x = targetPoint.x + ((center.x - targetPoint.x) * 2);
 			}
 			else if(center.x < targetPoint.x){
@@ -163,6 +163,8 @@ public class Polygon {
 			result.y = targetPoint.y;
 		}
 		else {  //기울기가 수직이 아닐 때
+			      //http://blog.naver.com/terry422?Redirect=Log&logNo=130145856818 
+		          //공식 이용
 			result.x = (targetPoint.x * ((1) - (gradient*gradient))) - ((2*gradient) * (-1 * targetPoint.y + intercept));
 			result.x = result.x / ((gradient * gradient) + (1));
 			
@@ -193,35 +195,35 @@ public class Polygon {
 		float lGradient = getGradient(lStart, lEnd);
 		float lIntercept = getIntercept(lStart, lGradient);
 		
-		if (((lStart.x-lEnd.x)==0) && ((tlStart.y-tlEnd.y)==0))           //수직&수직
+		if (((lStart.x-lEnd.x)==0) && ((tlStart.y-tlEnd.y)==0))           //직선이 수직&터치 기울기가 수직
 			return null;
-		else if (((lStart.y-lEnd.y)==0) && ((tlStart.x-tlEnd.x)==0))    //수평&수평
+		else if (((lStart.y-lEnd.y)==0) && ((tlStart.x-tlEnd.x)==0))    //직선이 수평&터치 기울기가 수평
 			return null;
-		else if (((lStart.x-lEnd.x)==0) && ((tlStart.x-tlEnd.x)==0)){   //수직&수평
+		else if (((lStart.x-lEnd.x)==0) && ((tlStart.x-tlEnd.x)==0)){   //직선이 수직&터치 기울기가 수평
 			result.x = lStart.x;
 			result.y = center.y;
 		}
-		else if (((lStart.y-lEnd.y)==0) && ((tlStart.y-tlEnd.y)==0)){  //수평&수직
+		else if (((lStart.y-lEnd.y)==0) && ((tlStart.y-tlEnd.y)==0)){  //직선이 수평&터치 기울기가 수직
 			result.x = center.x;
 			result.y = lStart.y;
 		}
-		else if ((lStart.y-lEnd.y)==0){                                          //수평&대각선
+		else if ((lStart.y-lEnd.y)==0){                                          //직선이 수평&터치 기울기가 대각선
 			result.y = lStart.y;
 			result.x = (result.y + (tlGradient*center.x) - center.y) / tlGradient;
 		}
-		else if ((lStart.x-lEnd.x)==0){                                          //수직&대각선
+		else if ((lStart.x-lEnd.x)==0){                                          //직선이 수직&터치 기울기가 대각선
 			result.x = lStart.x;
 			result.y = (tlGradient * (result.x-center.x)) + center.y;
 		}
-		else if ((tlStart.x-tlEnd.x)==0){                                       //대각선&수평
+		else if ((tlStart.x-tlEnd.x)==0){                                       //직선이 대각선&터치 기울기가 수평
 			result.y = center.y;
 			result.x = (result.y - lIntercept) / lGradient; 			
 		}
-		else if ((tlStart.y-tlEnd.y)==0){                                      //대각선&수직
+		else if ((tlStart.y-tlEnd.y)==0){                                      //직선이 대각선&터치 기울기가 수직
 			result.x = center.x;
 			result.y = lGradient * result.x + lIntercept;
 		}
-		else{                                                                          //대각선&대각선
+		else{                                                                          //직선이 대각선&터치 기울기가 대각선
 			result.x = (center.y - (tlGradient*center.x) - lIntercept) / (lGradient - tlGradient);
 			result.y = lGradient * result.x + lIntercept;
 		}
@@ -241,24 +243,24 @@ public class Polygon {
 		if(cPoint == null)
 			return false;
 		
-		if((lStart.x-lEnd.x)==0){                                                         //수직일때
+		if((lStart.x-lEnd.x)==0){    //수직일때는 y값으로 비교
 			big = lStart.y;
 			small = lEnd.y;
 			mid = cPoint.y;
 		}
-		else{
+		else{                            //기울기가 있으면 x값으로 비교
 			big = lStart.x;
 			small = lEnd.x;
 			mid =cPoint.x;
 		}
 		
-		if(big<small){
+		if(big<small){              //big에다 큰 값 넣고 small에 작은 값 넣기
 			float temp = big;
 			big = small;
 			small = temp;
 		}
 		
-		if((big>mid) && (small<mid)){
+		if((big>mid) && (small<mid)){ //큰거보다 작고 작은거 보다 크면 가운데 있으니 선위에 존재한다
 			return true;
 		}
 		else{
@@ -280,21 +282,22 @@ public class Polygon {
 		PointF center = new PointF();                                          //터치 라인의 가운데 점
 		center = getCenterPoint(tlStart, tlEnd);  
 		
-		if ((tlStart.y-tlEnd.y)==0){                     //수직일때
+		//point와 tlStart(터치 입력시작점)가 기울기로 나뉘어진 두 면적중에서 같은면적에 있는지 확인
+		if ((tlStart.y-tlEnd.y)==0){   //입력이 수평. 따라서 만들어진 기울기는 수직. 수직일땐 무한값이라 계산이 안되서 따로 빼서 계산.
 			if(((point.x>center.x)&&(tlStart.x>center.x)) || ((point.x<center.x)&&(tlStart.x<center.x))){
-				return true;
+				return true;      //point와 tlStart의 x값이 둘다 수직 기울기의 x값보다 클 때나, 둘다 수직 기울기의 y값보다 작을때 같은 면적에 있다고 할 수 있다
 			}
 			else
 				return false;
 		}
-		else{
-			float pointOnGradY = tlGradient * (point.x - center.x) + center.y;
-			float tlStartOnGradY = tlGradient * (tlStart.x - center.x) + center.y;
+		else{  //기울기가 수직이 아닐때는 point.x 값과 tlStart.x 값을 기울기 공식에 대입하여 기울기에서 해당 x값에 대응하는 y값을 구한다.
+			float pointOnGradY = tlGradient * (point.x - center.x) + center.y;  //기울기에 point.x값을 대입해서 y값 구함
+			float tlStartOnGradY = tlGradient * (tlStart.x - center.x) + center.y; //기울기에 tlStart.x값을 대입해서 y값 구함
 			if( ((point.y>pointOnGradY)&&(tlStart.y>tlStartOnGradY)) || ((point.y<pointOnGradY)&&(tlStart.y<tlStartOnGradY)) ){
-				return true;
-			}
-			else
-				return false;
+				return true;    //구해진 y값 들 과 point.y, tlStart.y를 비교하여 point.y, tlStart.y가 둘다 기울기 보다 위에 있을때 
+			}                       //아니면 둘다 기울기 보다 아래 있을때 같은 면적에 있다고 보고 트루값을 리턴함.
+			else                   
+				return false; 
 		}
 	}
 	/**
