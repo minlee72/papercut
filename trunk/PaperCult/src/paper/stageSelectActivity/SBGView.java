0d0f895087c2e5a344ed45cf66f1b6644e2172a8
binan1 <binan1@21ed9746-ec09-4e3c-9cd8-fa4dcd8b6b0b>
2013-04-07 07:39:07 +0000

    스테이지 선택화면 만드는중

--- trunk/PaperCult/src/paper/stageSelectActivity/SBGView.java
+++ trunk/PaperCult/src/paper/stageSelectActivity/SBGView.java
@@ -2,6 +2,7 @@ package paper.stageSelectActivity;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.view.MotionEvent;
 
 public class SBGView extends GLSurfaceView
 {
@@ -16,4 +17,42 @@ public class SBGView extends GLSurfaceView
 		mContext = context;
 		sImg = img;
 	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event)
+    {
+		final int action = event.getAction();
+		
+		synchronized ( sImg.mGL )
+		{
+			sImg.TouchX = event.getX() * sImg.gInfo.ScalePx;
+			sImg.TouchY = event.getY() * sImg.gInfo.ScalePy;
+			
+			switch ( action & MotionEvent.ACTION_MASK )
+			{
+				case	MotionEvent.ACTION_DOWN	:
+						{
+							sImg.checkButton();
+							return true;
+						}
+				case	MotionEvent.ACTION_MOVE :
+						{
+							//sImg.gInfo.ListViewActionMove( sImg.TouchX, sImg.TouchY );
+						}
+				case	MotionEvent.ACTION_POINTER_DOWN	:
+						{
+							//sImg.PushButton( true );
+						}
+						break;
+		
+				case	MotionEvent.ACTION_UP :
+				case	MotionEvent.ACTION_POINTER_UP :
+						{
+
+						}
+						break;
+			}
+		}
+      	return true;
+    }
 }
