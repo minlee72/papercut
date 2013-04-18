9eb8af5fb6b5fe231661375072ae27000d1543a0
binan1 <binan1@21ed9746-ec09-4e3c-9cd8-fa4dcd8b6b0b>
2013-04-18 18:42:02 +0000

    이미지 변경 많이함

--- trunk/PaperCult/src/paper/data/StageData.java
+++ trunk/PaperCult/src/paper/data/StageData.java
@@ -18,7 +18,15 @@ public class StageData {
 		stp1.add((float)1, (float)0);
 		stp1.add((float)0, (float)1);
 		
-	
+		StagePolygon stp1_ = new StagePolygon();
+		stp1_.add((float)-0.1, (float)-0.1);
+		stp1_.add((float)1.2, (float)-0.1);
+		stp1_.add((float)-0.1, (float)1.2);
+		
+		Stage dum = new Stage(4, stp1, stp1_);
+		dum.titleImage = R.drawable.invisible;
+		dum.score = 0;
+		dum.locked = false;
 		
 		StagePolygon stp2 = new StagePolygon();		
 		stp2.add(0, 0);	
@@ -38,62 +46,111 @@ public class StageData {
 		stp2_.add((float)0.193, (float)0.843);
 		stp2_.add((float)0.193, (float)0.532);
 		
-		Stage stt = new Stage(4, stp2, stp2_);
-		stt.titleImage = R.drawable.s_name1;
-		stt.score = 0;
-		stt.locked = false;
-		
-		StagePolygon poly = new StagePolygon();
-		poly.add(0, 0);
-		poly.add((float)1, (float)0);
-		poly.add((float)0, (float)1);
-		
-		StagePolygon polyl = new StagePolygon();
-		polyl.add((float)-0.1, (float)-0.1);
-		polyl.add((float)1.2, (float)-0.1);
-		polyl.add((float)-0.1, (float)1.2);
-		
-		Stage dum = new Stage(4, poly, polyl);
-		dum.titleImage = R.drawable.invisible;
-		dum.score = 0;
-		dum.locked = false;
-		
-		Stage st = new Stage(4, poly, polyl);
+		Stage st = new Stage(4, stp2, stp2_);
 		st.titleImage = R.drawable.s_name1;
+		st.titleClearImage = R.drawable.s_name1_clear;
 		st.score = 0;
 		st.locked = false;
 		
-		Stage st1 = new Stage(5, poly, polyl);
-		st1.titleImage = R.drawable.s_name1;
-		st1.score = 0;
-		st1.locked = true;
+		StagePolygon stp3 = new StagePolygon();
+		stp3.add((float)0.183, (float)0.4802);
+		stp3.add((float)0.466, (float)0.4802);
+		stp3.add((float)0.5463, (float)0.3104);
+		stp3.add((float)0.86, (float)0.1923);
+		stp3.add((float)0.9662, (float)0.4802);
+		stp3.add((float)1, (float)0.4802);
+		stp3.add((float)1, (float)0.5634);
+		stp3.add((float)0.0319, (float)0.7394);
+		stp3.add((float)0, (float)0.7394);
+		stp3.add((float)0, (float)0.662);
 		
-		Stage st2 = new Stage(5, poly, polyl);
-		st2.titleImage = R.drawable.s_name1;
-		st2.score = 0;
-		st2.locked = true;
+		StagePolygon stp3_ = new StagePolygon();
+		stp3_.add((float)0.153, (float)0.4502);
+		stp3_.add((float)0.436, (float)0.4502);
+		stp3_.add((float)0.5263, (float)0.2804);
+		stp3_.add((float)0.88, (float)0.1623);
+		stp3_.add((float)0.9862, (float)0.4502);
+		stp3_.add((float)1.03, (float)0.4502);
+		stp3_.add((float)1.03, (float)0.5934);
+		stp3_.add((float)0.0339, (float)0.7694);
+		stp3_.add((float)-0.03, (float)0.7694);
+		stp3_.add((float)-0.03, (float)0.632);
 		
-		Stage st3 = new Stage(5, poly, polyl);
-		st3.titleImage = R.drawable.s_name1;
+		
+		Stage st3 = new Stage(5, stp3, stp3_);
+		st3.titleImage = R.drawable.s_name2;
+		st3.titleClearImage = R.drawable.s_name2_clear;
 		st3.score = 0;
-		st3.locked = true;
+		st3.locked = false;
 		
-		Stage st4 = new Stage(5, poly, polyl);
-		st4.titleImage = R.drawable.s_name1;
+		StagePolygon stp4 = new StagePolygon();
+		stp4.add((float)0.3, 0);
+		stp4.add((float)0.37, (float)-0.03);
+		stp4.add((float)0.4, (float)-0.1);
+		stp4.add((float)0.495, (float)-0.075);
+		stp4.add((float)0.57, (float)-0.1);
+		stp4.add((float)0.6, (float)-0.035);
+		stp4.add((float)0.7, (float)0);
+		stp4.add((float)0.515, (float)1);
+		stp4.add((float)0.485, (float)1);
+		
+		StagePolygon stp4_ = new StagePolygon();
+		stp4_.add((float)0.27, (float)-0.03);
+		stp4_.add((float)0.34, (float)-0.06);
+		stp4_.add((float)0.4, (float)-0.13);
+		stp4_.add((float)0.495, (float)-0.105);
+		stp4_.add((float)0.57, (float)-0.13);
+		stp4_.add((float)0.63, (float)-0.065);
+		stp4_.add((float)0.73, (float)-0.01);
+		stp4_.add((float)0.545, (float)1.03);
+		stp4_.add((float)0.455, (float)1.03);
+		
+		
+		Stage st4 = new Stage(5, stp4, stp4_);
+		st4.titleImage = R.drawable.s_name3;
+		st4.titleClearImage = R.drawable.s_name3_clear;
 		st4.score = 0;
-		st4.locked = true;
+		st4.locked = false;
 		
-		Stage st5 = new Stage(5, poly, polyl);
-		st5.titleImage = R.drawable.s_name1;
+		StagePolygon stp5 = new StagePolygon();
+		stp5.add((float)0.37, (float)0.36);
+		stp5.add((float)0.459, (float)0.236);
+		stp5.add((float)0.5, (float)0.2);
+		stp5.add((float)0.627, (float)0.267);
+		stp5.add((float)0.627, (float)0.475);
+		stp5.add((float)0.865, (float)0.475);
+		stp5.add((float)0.904, (float)0.403);
+		stp5.add((float)1, (float)0.475);
+		stp5.add((float)0.798, (float)0.602);
+		stp5.add((float)0.6267, (float)0.602);
+		stp5.add((float)0.5, (float)0.475);
+		stp5.add((float)0.5, (float)0.357);
+		stp5.add((float)0.407, (float)0.415);
+		
+		StagePolygon stp5_ = new StagePolygon();
+		stp5_.add((float)0.33, (float)0.33);
+		stp5_.add((float)0.449, (float)0.206);
+		stp5_.add((float)0.5, (float)0.17);
+		stp5_.add((float)0.657, (float)0.267);
+		stp5_.add((float)0.657, (float)0.445);
+		stp5_.add((float)0.835, (float)0.445);
+		stp5_.add((float)0.904, (float)0.373);
+		stp5_.add((float)1.03, (float)0.475);
+		stp5_.add((float)0.828, (float)0.632);
+		stp5_.add((float)0.5967, (float)0.632);
+		stp5_.add((float)0.47, (float)0.475);
+		stp5_.add((float)0.47, (float)0.397);
+		stp5_.add((float)0.397, (float)0.445);
+		
+		Stage st5 = new Stage(5, stp5, stp5_);
+		st5.titleImage = R.drawable.s_name4;
+		st5.titleClearImage = R.drawable.s_name4_clear;
 		st5.score = 0;
-		st5.locked = true;
+		st5.locked = false;
 		
 		list.add(dum);
 		list.add(dum);
-		list.add(stt);
 		list.add(st);
-		list.add(st1);
-		list.add(st2);
 		list.add(st3);
 		list.add(st4);
 		list.add(st5);
--- trunk/PaperCult/src/paper/stageSelectActivity/SFGView.java
+++ trunk/PaperCult/src/paper/stageSelectActivity/SFGView.java
@@ -16,11 +16,13 @@ public class SFGView extends View {
 	}
 
 	public void onDraw(Canvas canvas){
+		/*
 		Paint paint = new Paint();
 		paint.setStyle(Paint.Style.FILL);
 		paint.setColor(0x40000000);
 		canvas.drawRect(0, 0, scrWidth, (scrHeight/8)*3, paint);
 		canvas.drawRect(0,(scrHeight/8)*5, scrWidth, scrHeight, paint);
+		*/
 	}
 
 }
